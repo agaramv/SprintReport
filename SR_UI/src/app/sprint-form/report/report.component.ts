46bc7f14341660c0545d1831888c0a59c88030de
agaramv <42156415+agaramv@users.noreply.github.com>
2020-04-30 17:42:27 -0400

    Datepicker, Working form
    I implemented the data picker elements and made sure the form works

--- SR_UI/src/app/sprint-form/report/report.component.ts
+++ SR_UI/src/app/sprint-form/report/report.component.ts
@@ -3,6 +3,7 @@ import { NgForm, FormControl, FormGroup } from '@angular/forms';
 import { SprintFormService } from '../sprint-form.service';
 import { Reason } from 'src/app/Models/reasons.model';
 import { Team } from 'src/app/Models/team.model';
+import { sprintReport } from 'src/app/Models/sprintReport.model';
 
 @Component({
   selector: 'app-report',
@@ -13,10 +14,20 @@ export class ReportComponent implements OnInit {
   submitted:boolean = false;
   otr = new FormControl();
   otrList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  otrSelected: string[];
   itr = new FormControl();
   itrList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  sprintCategory:String[] = ['Forecast', 'Capacity','Estimated','Completed']
   reasons:Reason[]
   teams:Team[]
+  otherEnabled=false;
+
+  origPbiPerc:number=3;
+  origBugPerc:number;
+  addPbiPerc:number;
+  addBugPerc:number;
+  sprintReport: any; //Object to take form
   
   constructor(private sprintFormService: SprintFormService) { }
   ngOnInit(): void {
@@ -26,23 +37,25 @@ export class ReportComponent implements OnInit {
 
   getReasons(){
     this.sprintFormService.getReasons().subscribe((data: Reason[])=>{
-      console.log(data);
+      // console.log(data);
       this.reasons = data;
-      console.log(this.reasons)
+      // console.log(this.reasons)
     })
   }
 
   getTeams(){
     this.sprintFormService.getTeams().subscribe((data: Team[])=>{
-      console.log(data);
+      // console.log(data);
       this.teams = data;
-      console.log(this.teams)
+      // console.log(this.teams)
     })
   }
 
   //Captures the form data
   onSubmit(report:NgForm) {
-    console.log(report.value)
+    //need to change the report.value.sprintEnd and Start to the data format we want
+    this.sprintReport = JSON.stringify(report.value);
+    console.log(this.sprintReport)
   }
 
 }
